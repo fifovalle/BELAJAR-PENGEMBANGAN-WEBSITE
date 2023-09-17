36c507a11de882e96ab5e580310b398a8b834fe2
fifovalle <fifanaufal10@gmail.com>
2023-09-17 13:07:30 +0700

    Menambahkan file basic utility bagian 2

---
+++ 6 FRAMEWORK/3 TAILWIND CSS/3 BASIC UTILITY BAGIAN 2/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4D5AE5",
+        secondary: "#E7E9FC",
+      },
+      spacing: {
+        13: "3.25rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+    },
+  },
+  plugins: [],
+};
