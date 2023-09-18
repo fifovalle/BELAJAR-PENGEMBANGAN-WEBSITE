dd3889383c1dc9eabb3fbde527d8dbebbb32e739
fifovalle <fifanaufal10@gmail.com>
2023-09-18 12:51:39 +0700

    Menambahkan file layouting

---
+++ 6 FRAMEWORK/3 TAILWIND CSS/6 LAYOUTING/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4D5AE5",
+        secondary: "#E7E9FC",
+      },
+      spacing: {
+        13: "3.25rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+    },
+  },
+  plugins: [],
+};
